8848fc722486d8ef0732f2d30af7caf71918c969
Kevin Neuenfeldt <kevin.neuenfeldt@googlemail.com>
2013-11-12 10:42:17 +0100

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :issue_repo_history_merge do
+  name 'Issue Repo History Merge plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
