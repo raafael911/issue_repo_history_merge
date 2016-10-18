ba22e8ec6038e0a9794df4d5757ac9ef8b823135
Kevin Neuenfeldt <kevin.neuenfeldt@enervision.de>
2016-10-18 10:39:55 +0200

    :except field for :authorize in IssuesControllerPath fixed

--- lib/repo_merge_issues_controller_patch.rb
+++ lib/repo_merge_issues_controller_patch.rb
@@ -1,43 +1,43 @@
 require_dependency 'issues_controller'
 
-module IssuesControllerPatch  
-  
+module IssuesControllerPatch
+
   def self.included(base)
     base.extend(ClassMethods)
-    
+
     base.send(:include, InstanceMethods)
-    
+
     base.class_eval do
       unloadable
-      before_filter :authorize, :except => [:index, :toggle_revisions, :toggle_relations]
+      before_filter :authorize, :except => [:index, :new, :create, :toggle_revisions, :toggle_relations]
     end
-    
+
   end
-  
+
   module ClassMethods
-    
+
   end
-  
+
   module InstanceMethods
-    
+
     def toggle_relations
       session[:show_relations] = !session[:show_relations]
-        
+
       respond_to do |format|
         format.js { render nothing: true }
       end
     end
-    
-    def toggle_revisions 
+
+    def toggle_revisions
       session[:show_revisions] = !session[:show_revisions]
-        
+
       respond_to do |format|
         format.js { render nothing: true }
-      end      
+      end
     end
-    
+
   end
-  
+
   IssuesController.send(:include, IssuesControllerPatch)
-  
+
 end
